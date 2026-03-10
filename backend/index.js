2954c36fd6456aa08d34d9dd9474d434c0e8f9e5
Your Name <pandeyangel986@gmail.com>
2026-03-10 22:31:19 +0545

    committed

--- backend/index.js
+++ backend/index.js
@@ -4,9 +4,28 @@ require('dotenv').config();
 const cors=require('cors')
 const port = process.env.PORT || 5000;
 
+let isConnected=false;
+async function connectToMonngoDB(){
+  try{ 
+    await mongoose.connect(process.env.MONGO_URI,{
+      useNewUrlParser:true,
+      useUnifiedTopology:true
+    }); 
+    isConnected=true;
+    console.log('Connected to MongoDB');
+  } catch(error){
+    console.error('Error connecting to MongoDB:',error);
+  }
+}
 // Middleware
 app.use(cors())
 app.use(express.json());
+app.use((req,res,next)=>{
+    if(!isConnected){
+      connectToMonngoDB();
+    }
+    next();
+})
 
 // Connect to DB
 const connectDB = require('./config/db');
@@ -25,6 +44,7 @@ app.get('/', (req, res) => {
 });
 
 // Start server
-app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
-});
+// app.listen(port, () => {
+  //console.log(`Server running on port ${port}`);
+//});
+module.exports=app
